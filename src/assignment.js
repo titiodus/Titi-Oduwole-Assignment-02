06f47b0a028cef3920f0934a4d82056d6649b2b1
Titilola Oduwole <titioduwole@gmail.com>
2022-07-02 10:03:58 +0000

    Attempted Challenge 1 and Challenge 2. Please check them. Thank you

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,19 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+  
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i]
+    }
+    console.log(sum)
+    return sum
+    // return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+sumOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 /**
  * Challenge - 2
@@ -27,10 +36,21 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+function countEvenNumbers(arrayOfNumbersEven) {
+  
+    const evenNumber = [];
+
+  arrayOfNumbersEven.forEach(number => {
+    if (number % 2 === 0) {
+      evenNumber.push(number)
+    }
+  })
+    console.log(evenNumber)
+    return evenNumber
+    // return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
 
 /**
  * Challenge - 3
